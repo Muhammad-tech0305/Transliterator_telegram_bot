688fb6c69407a11c0e33807f34f58a5791367849
Muhammadjon <muhammadyashnar2003@gmail.com>
2025-03-28 08:47:58 +0500

    ..

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import sys
+print(sys.executable)
 
 from transliterate import to_cyrillic, to_latin
+import telebot
+
+TOKEN = '7300811741:AAFnoU5oYx_N4hOxs0fEw6tjR7Qhk2VAVp8'
+bot = telebot.TeleBot(TOKEN, parse_mode=None)
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+	bot.reply_to(message, "Assalomu a'laykum! Xush kelibsiz! ")
+
+bot.polling()
+
+
+
+
 
 matn = input("Matn kiriting: ").lower()
 
